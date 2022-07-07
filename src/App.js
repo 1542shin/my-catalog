fbb9cfb4ec54830c99eb5e5756ad185b064dcd6a
Shin <1542shin@gmail.com>
2022-07-07 11:39:34 -0400

    move aos from js file

--- src/App.js
+++ src/App.js
@@ -3,12 +3,8 @@ import { Sidebar } from "./feature/Sidebar";
 import Feed from "./feature/Feed";
 import "./index.css";
 import TopBar from "./feature/TopBar";
-import AOS from "aos";
-import "aos/dist/aos.css";
 
 function App() {
-  AOS.init();
-
   return (
     <div id="App">
       <TopBar/>
